1203429a787dd6451e987a59f3f99a77b6c355b0
Lucia Caro <74767567+luciiaaa@users.noreply.github.com>
2022-09-30 17:55:44 +0000

    basis van menu

--- main.py
+++ main.py
@@ -15,3 +15,32 @@ class MyAgent(MLAgent):
  
 my_agent = MyAgent()
 start(player_o=my_agent)
+
+def main ():
+  print("Kies je spel")
+  print()
+
+  choice = input("""
+                    A: Tegen een ander speler
+                    B: Makkelijk spel
+                    C: Moeilijk spel
+                    D: Plot de grafiek
+                    E: Train de tegenstander
+                    
+                    
+                    Maak een keuze: """)
+  if choice == "A" or choice == "a":
+    AnderSpeler()
+  elif choice == "B" or choice == "b":
+    MakkelijkSpel()
+  elif choice == "C" or choice == "c":
+    MoelijkSpel()
+  elif choice == "D" or choice == "d":
+    Grafiek()
+  elif choice == "E" or choice == "e":
+    TrainOnly()
+  else:
+    print("kies tussen A, B, C of D")
+    print("probeer opnieuw")
+    main()
+
